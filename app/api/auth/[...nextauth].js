b6fb69c7366d6d3555d0832e0f18c0ad7acda7cf
Vaibhav Gupta <95957291+vaibhavgupta9877@users.noreply.github.com>
2023-11-13 17:19:23 +0530

    azure setup

---
+++ app/api/auth/[...nextauth].js
@@ -0,0 +1,9 @@
+import AzureADProvider from "next-auth/providers/azure-ad";
+
+providers: [
+  AzureADProvider({
+    clientId: process.env.AZURE_AD_CLIENT_ID,
+    clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
+    tenantId: process.env.AZURE_AD_TENANT_ID,
+  }),
+];
